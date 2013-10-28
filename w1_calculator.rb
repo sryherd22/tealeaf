a91252e775c2307401f95b4414da4597b1a7b03a
Sonya Ryherd <sryherd22@gmail.com>
2013-10-27 20:49:49 -0500

    made a few style changes

--- w1_calculator.rb
+++ w1_calculator.rb
@@ -1,29 +1,37 @@
 def say(msg)
-	puts "--- #{msg}"
+	puts "=> #{msg}"
 end
 
-def function
-	say "Please enter 1 2 3 or 4 for the operation that you would like to preform"
-	say "  1) Add"
-	say "  2) Subtract"
-	say "  3) Multiply"
-	say "  4) Divide"
+def get_operation
+	puts
+	say "Please enter 1, 2, 3, or 4 for the operation that you would like to preform"
+	puts
+	say "1) Add"
+	say "2) Subtract"
+	say "3) Multiply"
+	say "4) Divide"
+	puts
 end
 
+puts
 say "Hello, this application will add, substract, multiply, or divide any two numbers"
+puts
 say "Please enter the first number:"
 num1 = gets.chomp
+puts
 
 say "Please enter the second number:"
 num2 = gets.chomp
 
-function
+get_operation
 operation = gets.chomp
+puts
 
 until [ "1", "2", "3", "4" ].include?(operation) do 
-		say "INVALID ENTRY!!!"
-		function
+		say "#{operation} is an INVALID ENTRY!!!"
+		get_operation
 		operation = gets.chomp
+		puts
 end
 
 if operation == "1" 
