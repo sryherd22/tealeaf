5883d8c17a1afd566c495e9c53050e7daf609684
Sonya Ryherd <sryherd22@gmail.com>
2013-10-27 20:29:23 -0500

    finished calculator app

--- w1_calculator.rb
+++ w1_calculator.rb
@@ -10,8 +10,23 @@ def function
 	say "  4) Divide"
 end
 
-def results
-	if operation == "1" 
+say "Hello, this application will add, substract, multiply, or divide any two numbers"
+say "Please enter the first number:"
+num1 = gets.chomp
+
+say "Please enter the second number:"
+num2 = gets.chomp
+
+function
+operation = gets.chomp
+
+until [ "1", "2", "3", "4" ].include?(operation) do 
+		say "INVALID ENTRY!!!"
+		function
+		operation = gets.chomp
+end
+
+if operation == "1" 
 		results = num1.to_i + num2.to_i
 		operation = "Add"
 	elsif operation == "2"
@@ -24,60 +39,6 @@ def results
 		results = num1.to_f / num2.to_f
 		operation = "divided by"
 	else
-		while true
-			say "Invalid Entry!!!!"
-			function
-			operation = gets.chomp
-		break
-		end
-end
-
-
-say "Hello, this application will add, substract, multiply, or divide any two numbers"
-say "Please enter the first number:"
-num1 = gets.chomp
-
-say "Please enter the second number:"
-num2 = gets.chomp
-
-function
-operation = gets.chomp
-
-#if operation == "1" 
-#	results = num1.to_i + num2.to_i
-#	operation = "Add"
-#elsif operation == "2"
-#	results = num1.to_i - num2.to_i
-#	operation = "Subtract"
-#elsif operation == "3"
-#	results = num1.to_i * num2.to_i
-#	operation = "multiply"
-#elsif operation == "4"
-#	results = num1.to_f / num2.to_f
-#	operation = "divided by"
-#else
-#	while true
-#		say "Invalid entry!"
-#		function
-#		operation = gets.chomp
-#		if operation == "1" || operation == "2" || operation == "3" || operation == "4"
-#
-#			if operation == "1" 
-#					results = num1.to_i + num2.to_i
-#					operation = "Add"
-#				elsif operation == "2"
-#					results = num1.to_i - num2.to_i
-#					operation = "Subtract"
-#				elsif operation == "3"
-#					results = num1.to_i * num2.to_i
-#					operation = "multiply"
-#				elsif operation == "4"
-#					results = num1.to_f / num2.to_f
-#					operation = "divided by"
-#			end
-#		break		
-#		end
-#	end
 end
 
 say "#{num1} #{operation} #{num2} is: #{results}"
